7dafa6648ce12281205964a5f3678b9e93423f02
sajjadnejati98 <sajjadnejati.98@gmail.com>
2025-09-16 15:46:19 +0330

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.getenv("TOKEN")
+if not TOKEN:
+    raise ValueError("لطفاً TOKEN را در Environment Variables ست کنید!")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! ربات تستی فعال است ✅")
