d615d181be3af4d67d9a962cfc826890b64cb486
sajjadnejati98 <sajjadnejati.98@gmail.com>
2025-09-20 11:07:32 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-import asyncio
 
 TOKEN = "8208186251:AAGhImACKTeAa1pKT1cVSQEsqp0Vo2yk-2o"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات فعال است ✅")
 
-async def main():
+def main():
     app = Application.builder().token(TOKEN).build()
-
     app.add_handler(CommandHandler("start", start))
-
+    
     print("🚀 Bot is starting...")
-    await app.run_polling()
+    app.run_polling()  # بدون asyncio.run()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
